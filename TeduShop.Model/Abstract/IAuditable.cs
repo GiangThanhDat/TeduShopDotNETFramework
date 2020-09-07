2b7816d226f8879d0c7402363f36ae4b9f74a4b8
Giang Thanh Dat <dat.giang@mekong-tech.com>
2020-09-08 03:12:58 +0700

    Add model files

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Model
+{
+    interface IAuditable
+    {
+        DateTime? CreateDate { set; get; }
+        string CreateBy { get; set; }
+        DateTime? UpdateDate { set; get; }
+        string UpdateBy { set; get; }
+        string MetaKeyword { set; get; }
+        string MetaDescription { set; get; }
+        bool Status { set; get; }
+    }
+}
